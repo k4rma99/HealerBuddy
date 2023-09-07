f87eb8ba8f3d6c166bf03b814fc4a41d3498b4b3
k4rma99 <k4.rma321@gmail.com>
2023-09-07 16:53:31 +0530

    Main logo redirect to homepage

--- src/shared/header.tsx
+++ src/shared/header.tsx
@@ -8,13 +8,13 @@ export default function Header() {
     return (
         <header className="text-gray-600 body-font pr-20 pl-20">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <Image
                         src={img}
                         alt="logo"
                         height={40}
                     />
-                </a>
+                </Link>
                 <div className="flex flex-row ">
                     <Link href={"/for-buddies"}>
                         <button className="inline-flex items-center rounded text-base mt-4 md:mt-0 mr-5">For Buddies</button>
