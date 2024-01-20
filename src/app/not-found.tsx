83e00c1c03d2ce5d28fade51cc83390f792fddf8
k4rma99 <k4.rma321@gmail.com>
2024-01-21 03:53:28 +0530

    Page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,8 @@ const NotFound = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="max-w-md rounded-lg bg-white p-8 text-center shadow-lg">
         <h1 className="text-4xl font-semibold mb-4">404</h1>
-        <p className="text-gray-500">Oops! The page you are looking for could not be found.</p>
-        <a href="/" className="mt-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600">
+        <p className="text-gray-500 mb-5">Oops! The page you are looking for could not be found.</p>
+        <a href="/" className="px-4 py-2 bg-fi-darker text-white font-semibold rounded-lg shadow-md hover:bg-opacity-80 transition-all duration-500">
           Go Back to Home
         </a>
       </div>
